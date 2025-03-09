4cc6a843cd4ec1ef9e30fbd5d4e304b102356d9e
456789123102 <aize.gong@gmail.com>
2025-03-09 15:15:59 +0700

    remove duplicate files from frontend

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode:false,
-  images: {
-    domains: ['firebasestorage.googleapis.com', 'lovedrinks.com'], // เพิ่มโดเมนของรูปภาพที่ต้องการใช้
-  },
-};
-
-export default nextConfig;
